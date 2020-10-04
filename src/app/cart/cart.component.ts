7af3348d30fcf2ff93832d80c993bb8235b9f252
bovao <alexis.grimonprez@gmail.com>
2020-10-04 19:04:14 +0200

    Add form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
+
 
 
 @Component({
@@ -9,14 +12,28 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items; //propriété pour stocker les produits dans le panier.
+  checkoutForm; //stocker le modèle de formulaire.
 
   constructor(
-    //Inject the CartService so that the cart component can use it
+    private formBuilder: FormBuilder,
     private cartService: CartService
-  ) { }
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
   }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+    console.warn('Your order has been submitted', customerData);
+  }
+
 
 }
